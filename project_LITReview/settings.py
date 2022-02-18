d7b1fbbe6660ec379cb6977cb84009c467baa64d
byoso <emailvinz@gmail.com>
2022-02-18 15:22:44 +0100

    bulma integrated a better way

--- project_LITReview/settings.py
+++ project_LITReview/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party
-    'bulma',
     'crispy_forms',
 
     # local
@@ -125,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
