1220df7e0184391539e8558a0894ed1fc72e1a6d
byoso <emailvinz@gmail.com>
2022-02-19 03:57:28 +0100

    Material Design set up OK

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,12 +4,26 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
+    password1 = forms.CharField(
+        max_length=150, widget=forms.PasswordInput(attrs={'class': 'input'}))
+    password2 = forms.CharField(
+        max_length=150, widget=forms.PasswordInput(attrs={'class': 'input'}))
+
     class Meta:
         model = get_user_model()
-        fields = ('username',)
+        fields = ('username', 'password1', 'password2')
+        widgets = {
+            "username": forms.TextInput(
+                attrs={"class": "input"}),
+        }
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150, label="Nom d'utilisateur")
+    username = forms.CharField(
+        max_length=150, label="Nom d'utilisateur",
+        widget=forms.TextInput(
+            attrs={"class": "input"}))
     password = forms.CharField(
-        max_length=150, label="Mot de passe", widget=forms.PasswordInput)
+        max_length=150, label="Mot de passe",
+        widget=forms.PasswordInput(
+            attrs={"class": "input"}))
